89d47e648dad41fdc8e59d7172842c20472a4135
jonathan343 <jonathangee09@gmail.com>
2026-01-25 17:28:20 -0500

    Add TrackService with get and get_several operations

--- src/spotify_sdk/client.py
+++ src/spotify_sdk/client.py
@@ -6,6 +6,7 @@ from typing import Any
 
 from ._base_client import BaseClient
 from .services.albums import AlbumService
+from .services.tracks import TrackService
 
 
 class SpotifyClient:
@@ -43,6 +44,7 @@ class SpotifyClient:
 
         # Initialize services
         self.albums = AlbumService(self._base_client)
+        self.tracks = TrackService(self._base_client)
 
     def close(self) -> None:
         """Close the client and release resources."""
